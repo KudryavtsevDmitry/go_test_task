b2a45462d5fa97c1e61d44d1f32d86569dad905e
Kudryavtsev Dmitry <kudryavtsevda@yandex.ru>
2019-08-12 00:00:39 +0300

    Change file reading from os.File to bufio.Reader.

--- mp4parser.go
+++ mp4parser.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 // Atom representation
@@ -13,54 +15,84 @@ type Atom struct {
 	Pos      int64
 	Size     int64
 	AtomType string
+	Depth    int
 }
 
 var buf = make([]byte, 8)
 
 func main() {
 	mp4FilePath := os.Args[1]
-	file := openFile(mp4FilePath)
+	file, err := os.Open(mp4FilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := bufio.NewReader(file)
 
 	var absPosition int64
+	var seekError error
 	for {
-		absPosition = seekFile(file, absPosition)
-		if !readFile(file, buf) {
-			break
+
+		absPosition, seekError = file.Seek(absPosition, 0)
+		reader.Reset(file)
+		if seekError != nil {
+			log.Fatal(seekError)
+		}
+		b, err := reader.Read(buf)
+		if err != nil {
+			if err == io.EOF && b == 0 {
+				fmt.Println("All atoms succefully readed.",err)
+				break
+			}
 		}
 		atomSize := int64(binary.BigEndian.Uint32(buf[:8/2]))
 		atomType := string(buf[4:8])
-		atom := Atom{absPosition + 8, atomSize, atomType}
+		atom := Atom{absPosition + 8, atomSize, atomType, 0}
 		absPosition = absPosition + atomSize
-		showChildAtoms(file, atom)
+		showChildAtoms(file, reader, atom)
 	}
 }
 
-func showChildAtoms(file *os.File, atom Atom) {
+func showChildAtoms(file *os.File, reader *bufio.Reader, atom Atom) {
 	queue := []Atom{atom}
 	for len(queue) > 0 {
 		childs := []Atom{}
-		poppedAtom := queue[len(queue)-1]
-		queue = queue[:len(queue)-1]
+		poppedAtom := queue[0]
+		queue = queue[1:len(queue)]
+
 		printAtom(poppedAtom)
 		if !leafAtom(poppedAtom.AtomType) {
-			childs = getAtomChilds(file, poppedAtom)
+			childs, _ = getAtomChilds(file, reader, poppedAtom)
 		}
-		queue = append(queue, childs...)
+		queue = append(childs, queue...)
 	}
 }
 
-func getAtomChilds(file *os.File, parentAtom Atom) []Atom {
+func getAtomChilds(file *os.File, reader *bufio.Reader, parentAtom Atom) ([]Atom, error) {
 	childs := []Atom{}
+	var childsError error
+	var seekError error
+
 	absPosition := parentAtom.Pos
-	for true {
-		absPosition = seekFile(file, absPosition)
-		if !readFile(file, buf) {
+	for {
+		absPosition, seekError = file.Seek(absPosition, 0)
+		if seekError != nil {
+			childsError = seekError
 			break
+		}
+		reader.Reset(file)
+
+		b, readError := reader.Read(buf)
+		if readError != nil {
+			if readError == io.EOF && b == 0 {
+				childsError = readError
+				break
+			}
 		}
 		atomSize := int64(binary.BigEndian.Uint32(buf[:8/2]))
 		atomType := string(buf[4:8])
 
-		childAtom := Atom{absPosition + 8, atomSize, atomType}
+		childAtom := Atom{absPosition + 8, atomSize, atomType, parentAtom.Depth + 1}
 		absPosition = absPosition + atomSize
 
 		if childAtom.Size < parentAtom.Size && childAtom.Size != 0 {
@@ -69,39 +101,16 @@ func getAtomChilds(file *os.File, parentAtom Atom) []Atom {
 			break
 		}
 	}
-	return childs
-}
-
-func openFile(mp4FilePath string) *os.File {
-	file, err := os.Open(mp4FilePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return file
-}
-
-func seekFile(file *os.File, absPosition int64) int64 {
-	absPosition, seekError := file.Seek(absPosition, 0)
-	if seekError != nil {
-		log.Fatal(seekError)
-	}
-	return absPosition
-}
-
-func readFile(file *os.File, buf []byte) bool {
-	b, err := file.Read(buf)
-	if err != nil {
-		if err == io.EOF && b == 0 {
-			return false
-		}
-	}
-	return true
+	return childs, childsError
 }
 
 func printAtom(atom Atom) {
-	fmt.Print("Atom type: ", atom.AtomType)
+	depthTabs := strings.Repeat("\t", atom.Depth)
+
+	fmt.Print(depthTabs+"Atom type: ", atom.AtomType)
 	fmt.Println(" size:", atom.Size)
 }
+
 func leafAtom(lookup string) bool {
 	switch lookup {
 	case
